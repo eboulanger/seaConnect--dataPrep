ede530d6045aae9c8c593c40607b2b95014cf627
eboulanger <emilie.boulanger@hotmail.com>
2019-07-15 13:21:17 +0200

    scripts and results update

--- 01-SNPfilters/filtering_mul.sh
+++ 01-SNPfilters/filtering_mul.sh
@@ -7,7 +7,7 @@ mkdir 02-Mullus
 cd 02-Mullus
 
 FILE=../../00-rawData/02-Mullus/mullus.vcf
-# PATH=$PATH:~/programmes/vcflib/bin/
+PATH=$PATH:~/programmes/vcflib/bin/
 
 # step 1: filter genotypes called below 50% (across all individuals), 
 #         filter SNPs that have a minor allele count less than 3
@@ -52,7 +52,7 @@ vcftools --vcf mullus.g5mac3dp3.recode.vcf --remove lowDP.indv --recode --recode
 
 # step 4 : restrict data to variants called in a high percentage of individuals and filter by mean depth of genotypes
 
-vcftools --vcf mullus.g5mac3dplm.recode.vcf --max-missing 0.95 --maf 0.05 --recode --recode-INFO-all --out DP3g95maf05 --min-meanDP 0
+vcftools --vcf mullus.g5mac3dplm.recode.vcf --max-missing 0.95 --maf 0.05 --recode --recode-INFO-all --out DP3g95maf05 --min-meanDP 5
 
 ###
 # FreeBayes - specific filters
@@ -63,7 +63,7 @@ awk '/#/' DP3g95maf05.recode.vcf
 # step 5 : filter for allele balance
 #          this requires vcflib
 
-vcffilter -s -f "AB > 0.25 & AB < 0.75 | AB < 0.01" DP3g95p5maf05.recode.vcf > DP3g95p5maf05.fil1.vcf
+# population-level: vcffilter -s -f "AB > 0.25 & AB < 0.75 | AB < 0.01" DP3g95p5maf05.recode.vcf > DP3g95p5maf05.fil1.vcf
 vcffilter -s -f "AB > 0.25 & AB < 0.75 | AB < 0.01" DP3g95maf05.recode.vcf > DP3g95maf05.fil1.vcf
 # check how many were removed
 awk '!/#/' DP3g95maf05.recode.vcf | wc -l
@@ -73,8 +73,9 @@ awk '!/#/' DP3g95maf05.fil1.vcf | wc -l
 vcffilter -f "SAF / SAR > 100 & SRF / SRR > 100 | SAR / SAF > 100 & SRR / SRF > 100" -s DP3g95maf05.fil1.vcf > DP3g95maf05.fil2.vcf
 awk '!/#/' DP3g95maf05.fil2.vcf | wc -l
 
+# SKIPPED STEP 6 FOR NOW
 # step 7 : look at ration of mapping qualities between reference and alternate alleles 
-vcffilter -f "MQM / MQMR > 0.9 & MQM / MQMR < 1.05" DP3g95maf05.fil2.vcf > DP3g95maf05.fil3.vcf
+vcffilter -f "MQM / MQMR > 0.9 & MQM / MQMR < 1.05" DP3g95maf05.fil1.vcf > DP3g95maf05.fil3.vcf
 awk '!/#/' DP3g95maf05.fil3.vcf | wc -l
 
 # step 8 : check whether there is discrepancy in the properly paired status (SNAP IK NIET MAAR MAAKT BLIJKBAAR NIET UIT WANT GEEN SNPs WEGGEFILTERD)
@@ -101,7 +102,7 @@ paste DP3g95maf05.fil5.vcf.loci.qual DP3g95maf05.fil5.DEPTH | awk -v x=$MEAN_DP_
 ## 6 : remove those sites and recalculate depth across loci
 vcftools --vcf DP3g95maf05.fil5.vcf --site-depth --exclude-positions DP3g95maf05.fil5.lowQDloci --out DP3g95maf05.fil5
 ## 7 : cut depth scores, calculate average depth and plot
-NINDIV=297
+NINDIV=424
 cut -f3 DP3g95maf05.fil5.ldepth > DP3g95maf05.fil5.site.depth
 awk '!/D/' DP3g95maf05.fil5.site.depth | awk -v x=$NINDIV '{print $1/x}' > meandepthpersite
 gnuplot << \EOF 
@@ -139,10 +140,13 @@ TO DO
 
 TO DO
 
-# step 3 : outliers
+# step 13 : outliers
 
 TO DO
 
+# step 14 : rename final filtered dataset
+
+cp DP3g95maf05.FIL.hwe.recode.vcf mullus_all_filtered.vcf
 
 
 
